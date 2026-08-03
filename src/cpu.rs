48894c4c4f973d66b68eeb879522b87885550313
zocomputer <help@zocomputer.com>
2026-08-03 12:09:18 +0000

    Fix A64 memory callback compilation

--- src/cpu.rs
+++ src/cpu.rs
@@ -95,7 +95,8 @@ extern "C" fn touchHLE_cpu_write_u64(mem: *mut touchHLE_Mem, addr: VAddr, value:
 fn touchHLE_cpu_read_64_impl<T: SafeRead + Default>(mem: *mut touchHLE_Mem, addr: u64, error: *mut bool) -> T {
     let res = std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
         let mem = unsafe { &mut *mem.cast::<Mem>() };
-        mem.read(Ptr::from_bits(addr))
+        let addr = u32::try_from(addr).expect("A64 guest address exceeds the current guest address space");
+        mem.read(Ptr::from_bits(addr.try_into().unwrap()))
     }));
     unsafe { error.write(res.is_err()); }
     res.unwrap_or_default()
@@ -104,7 +105,8 @@ fn touchHLE_cpu_read_64_impl<T: SafeRead + Default>(mem: *mut touchHLE_Mem, addr
 fn touchHLE_cpu_write_64_impl<T: SafeWrite>(mem: *mut touchHLE_Mem, addr: u64, value: T) -> bool {
     std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
         let mem = unsafe { &mut *mem.cast::<Mem>() };
-        mem.write(Ptr::from_bits(addr), value)
+        let addr = u32::try_from(addr).expect("A64 guest address exceeds the current guest address space");
+        mem.write(Ptr::from_bits(addr.try_into().unwrap()), value)
     })).is_err()
 }
 
--- src/mem.rs
+++ src/mem.rs
@@ -296,6 +296,7 @@ unsafe impl<T, const MUT: bool> SafeRead for Ptr<T, MUT> {}
 ///
 /// See also [SafeRead] and [crate::abi].
 pub trait SafeWrite: Sized {}
+impl SafeRead for [u64; 2] {}
 impl<T: SafeRead> SafeWrite for T {}
 
 type Bytes = [u8; 1 << 32];
